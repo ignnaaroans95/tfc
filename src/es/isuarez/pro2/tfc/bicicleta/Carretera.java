7de9076cb3052b857a64325ef3938f334cf78ef4
ignnaaroans95 <1995igna@gmail.com>
2024-09-05 10:53:10 +0200

    Implementación de atributos , comentarios y validación Carretera

--- src/es/isuarez/pro2/tfc/bicicleta/Carretera.java
+++ src/es/isuarez/pro2/tfc/bicicleta/Carretera.java
@@ -1,4 +1,56 @@
 package es.isuarez.pro2.tfc.bicicleta;
 
+import es.isuarez.pro2.tfc.bicicleta.enums.Color;
+import es.isuarez.pro2.tfc.bicicleta.enums.Fabricante;
+import es.isuarez.pro2.tfc.bicicleta.enums.Material;
+
 public class Carretera extends Bicicleta {
+
+
+    private float PesoTotal;
+    /**
+     * Constructor de la clase con todos los atributos heredados de la clase padre y su específico
+     * correspondiente
+     **/
+    public Carretera(String talla, boolean disponible, String pesoCuadro,
+                     double precio, int id, int ultimoId, Fabricante fabricante,
+                     Material material, Color color, float pesoTotal) {
+        super(talla, disponible, pesoCuadro, precio, id, ultimoId, fabricante, material, color);
+        PesoTotal = pesoTotal;
+    }
+    /**
+     * Obtiene el valor del peso total de la bicicleta.
+     *
+     * @return el peso total de la bicicleta.
+     */
+    public float getPesoTotal() {
+        return PesoTotal;
+    }
+
+    /**
+     * Asigna el valor de la bicicleta en caso de que sea necesario
+     *
+     * @param pesoTotal el nuevo valor para el peso total.
+     */
+
+    public void setPesoTotal(float pesoTotal) {
+        PesoTotal = pesoTotal;
+    }
+    /**Método toString() con validador implementado para evitar el NullPointerExceotion.En caso
+     * de que el valor sea nulo **/
+    @Override
+    public String toString() {
+        try {
+
+            if (this.getPesoTotal() == Float.parseFloat(null)) {
+                return "Peso total no disponible";
+            }
+
+            return super.toString() + "Peso total de la bicicleta: " + this.getPesoTotal();
+        } catch (NullPointerException e) {
+            // Capturar y manejar la excepción
+            return "Error: No se ha podido obtener el peso de la bicicleta";
+        }
+    }
+
 }
