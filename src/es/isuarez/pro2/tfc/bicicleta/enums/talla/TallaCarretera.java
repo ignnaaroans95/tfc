8df5d57053614fc372e3a7ed3a1868040bb00f3a
ignnaaroans95 <1995igna@gmail.com>
2024-09-16 10:48:24 +0200

    Enums de tallas añadidos con sus validaciones correspopndientes

---
+++ src/es/isuarez/pro2/tfc/bicicleta/enums/talla/TallaCarretera.java
@@ -0,0 +1,49 @@
+package es.isuarez.pro2.tfc.bicicleta.enums.talla;
+
+/**
+ * Respresenta el tamaño del cuadro de la bicicleta en función a la
+ * lungitud de la entrepierna del usuario. Todas ellas estarán correspondidas entre dos valores
+ * .Mínimo y máximo a través de constantes.
+ **/
+
+public enum TallaCarretera {
+    XS_S(47, 51),
+    S_M(51, 53),
+    M_L(53, 57),
+    L(57, 59),
+    L_XL(59, 61),
+    XL(61, Integer.MAX_VALUE);
+
+    private final int minTalla;
+    private final int maxTalla;
+
+    TallaCarretera(int minTalla, int maxTalla) {
+        this.minTalla = minTalla;
+        this.maxTalla = maxTalla;
+    }
+
+    public int getMinTalla() {
+        return minTalla;
+    }
+
+    public int getMaxTalla() {
+        return maxTalla;
+    }
+
+    /**
+     *
+     *  Método estático que valida los datos introducidos. De esta manera nos aseguramosque los datos introducidos por
+     *  el usuario son los adecuados.
+     *
+     */
+    public static TallaCarretera validarTalla(int talla) {
+        for (TallaCarretera tallaCarretera : TallaCarretera.values()) {
+            if (talla >= tallaCarretera.getMinTalla() && talla <= tallaCarretera.getMaxTalla()) {
+                return tallaCarretera;
+            }
+        }
+        throw new IllegalArgumentException("Talla no válida para bicicleta de carretera. Introduzca valores comprendidos" +
+                "entre la talla 41 y la talla 61,.");
+    }
+
+}
---
+++ src/es/isuarez/pro2/tfc/bicicleta/enums/talla/TallaMTB.java
@@ -0,0 +1,52 @@
+package es.isuarez.pro2.tfc.bicicleta.enums.talla;
+
+
+/**
+ * Respresenta el tamaño del cuadro de la bicicleta de montaña en función a la
+ * lungitud de la entrepierna del usuario. Todas ellas estarán correspondidas entre dos valores
+ * .Mínimo y máximo a través de constantes.
+ **/
+
+
+public enum TallaMTB {
+    XS_S(14, 16),
+    S_M(16, 17),
+    M_L(17, 20),
+    L(20, 22),
+    XL(22, Integer.MAX_VALUE);
+
+    private final int minTalla;
+    private final int maxTalla;
+
+    TallaMTB(int minTalla, int maxTalla) {
+        this.minTalla = minTalla;
+        this.maxTalla = maxTalla;
+    }
+
+    public int getMinTalla() {
+        return minTalla;
+    }
+
+    public int getMaxTalla() {
+        return maxTalla;
+    }
+
+    /**
+     *
+     *  Método estático que valida los datos introducidos. De esta manera nos aseguramosque los datos introducidos por
+     *  el usuario son los adecuados.
+     *
+     */
+
+
+    public static TallaMTB validarTalla(int talla) {
+        for (TallaMTB tallaMTB : TallaMTB.values()) {
+            if (talla >= tallaMTB.getMinTalla() && talla <= tallaMTB.getMaxTalla()) {
+                return tallaMTB;
+            }
+        }
+        throw new IllegalArgumentException("Talla no válida para bicicleta MTB.");
+    }
+}
+
+
