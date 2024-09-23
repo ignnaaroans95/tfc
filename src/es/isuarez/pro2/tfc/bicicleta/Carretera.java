ed92849d51836c0b9bc41b9047fc6d54973f8783
ignnaaroans95 <1995igna@gmail.com>
2024-09-23 11:23:42 +0200

    Modificados los comentarios y añadidos enumeradores específicos de las tallas -> Carretera y Mtb

--- src/es/isuarez/pro2/tfc/bicicleta/Carretera.java
+++ src/es/isuarez/pro2/tfc/bicicleta/Carretera.java
@@ -2,26 +2,53 @@ package es.isuarez.pro2.tfc.bicicleta;
 
 import es.isuarez.pro2.tfc.bicicleta.enums.caracteristicas.*;
 import es.isuarez.pro2.tfc.bicicleta.enums.caracteristicas.talla.DiametroRuedaBicicleta;
+import es.isuarez.pro2.tfc.bicicleta.enums.caracteristicas.talla.TallaCarretera;
 import es.isuarez.pro2.tfc.bicicleta.enums.persona.Genero;
 
+/**
+ * La clase {@code Carretera} representa una bicicleta de carretera, que hereda de la clase {@link Bicicleta}.
+ * Añade atributos específicos para este tipo de bicicleta como el peso total y la talla de carretera.
+ */
 public class Carretera extends Bicicleta {
 
-
+    /**
+     * Peso total de la bicicleta de carretera.
+     */
     private float pesoTotal;
 
     /**
-     * Constructor de la clase con todos los atributos heredados de la clase padre y su específico
-     * correspondiente
-     **/
-    public Carretera(String talla, boolean disponible, String pesoCuadro,
-                     double precio, int id, int ultimoId, Fabricante fabricante, DiametroRuedaBicicleta diametrorueda,
-                     Material material, Color color, Genero genero, float pesoTotal) {
-        super(talla, disponible, pesoCuadro, precio, id, ultimoId, fabricante,diametrorueda, material, color, genero);
+     * Talla específica para bicicletas de carretera, basada en la enumeración {@link TallaCarretera}.
+     */
+    private TallaCarretera tallaCarretera;
+
+    /**
+     * Constructor de la clase {@code Carretera}. Hereda los atributos de la clase {@link Bicicleta}
+     * y añade los atributos específicos de bicicletas de carretera.
+     *
+     * @param talla          la talla general de la bicicleta
+     * @param disponible     si la bicicleta está disponible para la venta
+     * @param pesoCuadro     el peso del cuadro de la bicicleta
+     * @param precio         el precio de la bicicleta
+     * @param id             el identificador único de la bicicleta
+     * @param ultimoId       el último identificador utilizado
+     * @param fabricante     el fabricante de la bicicleta
+     * @param diametrorueda  el diámetro de las ruedas de la bicicleta
+     * @param material       el material del cuadro de la bicicleta
+     * @param color          el color de la bicicleta
+     * @param genero         el género para el cual está diseñada la bicicleta
+     * @param pesoTotal      el peso total de la bicicleta
+     * @param tallaCarretera la talla específica para bicicletas de carretera
+     */
+    public Carretera(String talla, boolean disponible, String pesoCuadro, double precio, int id, int ultimoId,
+                     Fabricante fabricante, DiametroRuedaBicicleta diametrorueda, Material material, Color color,
+                     Genero genero, float pesoTotal, TallaCarretera tallaCarretera) {
+        super(talla, disponible, pesoCuadro, precio, id, ultimoId, fabricante, diametrorueda, material, color, genero);
         this.pesoTotal = pesoTotal;
+        this.tallaCarretera = tallaCarretera;
     }
 
     /**
-     * Obtiene el valor del peso total de la bicicleta.
+     * Obtiene el peso total de la bicicleta de carretera.
      *
      * @return el peso total de la bicicleta.
      */
@@ -30,33 +57,52 @@ public class Carretera extends Bicicleta {
     }
 
     /**
-     * Asigna el valor de la bicicleta en caso de que sea necesario
+     * Establece un nuevo valor para el peso total de la bicicleta.
      *
      * @param pesoTotal el nuevo valor para el peso total.
      */
-
     public void setPesoTotal(float pesoTotal) {
-        pesoTotal = pesoTotal;
+        this.pesoTotal = pesoTotal;
     }
 
     /**
-     * Método toString() con validador implementado para evitar el NullPointerExceotion.En caso
-     * de que el valor sea nulo
-     **/
+     * Obtiene la talla específica de la bicicleta de carretera.
+     *
+     * @return un valor de {@link TallaCarretera} que indica la talla de la bicicleta.
+     */
+    public TallaCarretera getTallaCarretera() {
+        return tallaCarretera;
+    }
+
+    /**
+     * Establece la talla específica de la bicicleta de carretera.
+     *
+     * @param tallaCarretera la talla de la bicicleta, basada en {@link TallaCarretera}.
+     */
+    public void setTallaCarretera(TallaCarretera tallaCarretera) {
+        this.tallaCarretera = tallaCarretera;
+    }
+
+    /**
+     * Método sobrescrito que devuelve una representación en cadena de los datos de la bicicleta de carretera,
+     * incluyendo los atributos heredados de {@link Bicicleta} y los propios de la clase {@code Carretera}.
+     * Se implementa un manejador para evitar un {@link NullPointerException} si alguno de los valores es nulo.
+     *
+     * @return una cadena que representa la información detallada de la bicicleta de carretera.
+     */
     @Override
     public String toString() {
         try {
-
+            // Validación para evitar errores en caso de valor nulo
             if (this.getPesoTotal() == Float.parseFloat(null)) {
                 return "Peso total no disponible";
             }
 
-            return super.toString() + "Peso total de la bicicleta: " + this.getPesoTotal();
+            return super.toString() + "\nPeso total de la bicicleta: " + this.getPesoTotal() +
+                    "\nTalla de la bicicleta: " + this.getTallaCarretera();
         } catch (NullPointerException e) {
-            // Capturar y manejar la excepción
+            // Captura de la excepción en caso de que algún valor no esté inicializado
             return "Error: No se ha podido obtener el peso de la bicicleta";
         }
     }
-
-
 }
--- src/es/isuarez/pro2/tfc/bicicleta/Mtb.java
+++ src/es/isuarez/pro2/tfc/bicicleta/Mtb.java
@@ -2,73 +2,124 @@ package es.isuarez.pro2.tfc.bicicleta;
 
 import es.isuarez.pro2.tfc.bicicleta.enums.caracteristicas.*;
 import es.isuarez.pro2.tfc.bicicleta.enums.caracteristicas.talla.DiametroRuedaBicicleta;
+import es.isuarez.pro2.tfc.bicicleta.enums.caracteristicas.talla.TallaMTB;
 import es.isuarez.pro2.tfc.bicicleta.enums.persona.Genero;
 import es.isuarez.pro2.tfc.bicicleta.enums.tipoMTB.*;
 
+/**
+ * La clase {@code Mtb} representa una bicicleta de montaña, que hereda de la clase {@link Bicicleta}.
+ * Incluye atributos específicos para bicicletas de montaña como la cantidad de platos, el tipo de MTB, y la talla específica.
+ */
+public class Mtb extends Bicicleta {
 
-public class Mtb extends Bicicleta{
-
-
-/***En este caso nos cevolverá  así como el diámetro de la rueda y la cantidad de platos el tipo de mtb,  los cuales,
- *  serán devuelto como una constante, ya que son  tipos diferenciados y todos están estandarizados ..*/
-
-
+    /**
+     * Atributo que define la cantidad de platos de la bicicleta de montaña.
+     */
     private CantidadPlatos platos;
+
+    /**
+     * Atributo que define el tipo de bicicleta de montaña, basado en la enumeración {@link tipoMTB}.
+     */
     protected tipoMTB mtb;
 
-
-
     /**
-     * Constructor de la clase.Hereda todos los atributos de la clase padre -> Bicicleta . Además está compuesto
-     * por los atributos propios de la clase.
-     *
-     * @param talla
-     * @param disponible
-     * @param pesoCuadro
-     * @param precio
-     * @param id
-     * @param ultimoId
-     * @param fabricante
-     * @param material
-     * @param color
-     * @param genero
+     * Atributo que define la talla específica para bicicletas de montaña, basado en la enumeración {@link TallaMTB}.
      */
-    public Mtb(String talla, boolean disponible, String pesoCuadro, double precio, int id, int ultimoId, Fabricante fabricante, Material material, Color color, Genero genero, CantidadPlatos platos, DiametroRuedaBicicleta diametroRueda, tipoMTB mtb) {
-        super(talla, disponible, pesoCuadro, precio, id, ultimoId, fabricante,diametroRueda, material, color, genero);
-        this.platos=platos;
-        this.mtb=mtb;
+    private TallaMTB tallaMtb;
 
-    }
     /**
+     * Constructor de la clase {@code Mtb}. Hereda los atributos de la clase padre {@link Bicicleta}
+     * y define los atributos propios de las bicicletas de montaña.
      *
-     * @return Nos retorna la cantidad de platos de la bicicleta como una constante.
+     * @param talla        la talla general de la bicicleta
+     * @param disponible   si la bicicleta está disponible para la venta
+     * @param pesoCuadro   el peso del cuadro de la bicicleta
+     * @param precio       el precio de la bicicleta
+     * @param id           el identificador único de la bicicleta
+     * @param ultimoId     el último identificador utilizado
+     * @param fabricante   el fabricante de la bicicleta
+     * @param material     el material del cuadro de la bicicleta
+     * @param color        el color de la bicicleta
+     * @param genero       el género para el cual está diseñada la bicicleta
+     * @param platos       la cantidad de platos de la bicicleta
+     * @param diametroRueda el diámetro de las ruedas de la bicicleta
+     * @param mtb          el tipo de bicicleta de montaña
+     * @param tallaMtb     la talla específica para bicicletas de montaña
+     */
+    public Mtb(String talla, boolean disponible, String pesoCuadro, double precio, int id, int ultimoId,
+               Fabricante fabricante, Material material, Color color, Genero genero, CantidadPlatos platos,
+               DiametroRuedaBicicleta diametroRueda, tipoMTB mtb, TallaMTB tallaMtb) {
+        super(talla, disponible, pesoCuadro, precio, id, ultimoId, fabricante, diametroRueda, material, color, genero);
+        this.platos = platos;
+        this.mtb = mtb;
+        this.tallaMtb = tallaMtb;
+    }
+
+    /**
+     * Devuelve la cantidad de platos de la bicicleta de montaña.
+     *
+     * @return un valor de {@link CantidadPlatos} que indica la cantidad de platos.
      */
     public CantidadPlatos getPlatos() {
         return platos;
     }
-    /**
-     *
 
     /**
-    /***
+     * Establece la cantidad de platos de la bicicleta de montaña.
      *
-     * @return el tipo de mountain bike como una constante
+     * @param platos el número de platos, basado en {@link CantidadPlatos}.
+     */
+    public void setPlatos(CantidadPlatos platos) {
+        this.platos = platos;
+    }
+
+    /**
+     * Devuelve el tipo de bicicleta de montaña.
+     *
+     * @return un valor de {@link tipoMTB} que indica el tipo de MTB.
      */
     public tipoMTB getMtb() {
         return mtb;
     }
 
     /**
+     * Establece el tipo de bicicleta de montaña.
      *
-     *Método de sobreescritura que nos mostrará todos los datos de la clase y clase padre.
+     * @param mtb el tipo de bicicleta, basado en {@link tipoMTB}.
+     */
+    public void setMtb(tipoMTB mtb) {
+        this.mtb = mtb;
+    }
+
+    /**
+     * Devuelve la talla específica para bicicletas de montaña.
      *
-     * **/
+     * @return un valor de {@link TallaMTB} que indica la talla de la MTB.
+     */
+    public TallaMTB getTallaMtb() {
+        return tallaMtb;
+    }
+
+    /**
+     * Establece la talla específica para bicicletas de montaña.
+     *
+     * @param tallaMtb la talla de la bicicleta, basada en {@link TallaMTB}.
+     */
+    public void setTallaMtb(TallaMTB tallaMtb) {
+        this.tallaMtb = tallaMtb;
+    }
+
+    /**
+     * Método sobrescrito que devuelve una representación en cadena de los datos de la bicicleta de montaña,
+     * incluyendo los atributos heredados de {@link Bicicleta} y los propios de la clase {@code Mtb}.
+     *
+     * @return una cadena que representa la información detallada de la bicicleta de montaña.
+     */
     @Override
-   public String toString(){
-
-        return super.toString() + "\nPlatos: " + this.getPlatos() + "\nDiámetro de la rueda: " + this.getDiametroRueda() +
-                "\nTipo de MTB " + this.mtb;
-
-
+    public String toString() {
+        return super.toString() + "\nPlatos: " + this.getPlatos() +
+                "\nDiámetro de la rueda: " + this.getDiametroRueda() +
+                "\nTipo de MTB: " + this.mtb +
+                "\nTalla: " + this.getTallaMtb();
     }
 }
